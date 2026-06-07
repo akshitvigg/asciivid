fcdea61785d02a085fd54a115271dede60b3c7cb
Akshit Vig <akshitvig48@gmail.com>
2026-06-07 23:57:41 +0530

    image to ascii done

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let ramp = " .:-=+*#%@";
 
-    for (i, (x, y, pixel)) in gray.pixels().enumerate() {
+    let mut prev_y = 0;
+
+    for (x, y, pixel) in gray.pixels() {
         // println!("{}, {} {},  {:?}", i, x, y, pixel[0]);
-
-        if i == 10 {
-            break;
-        }
-
         let ramp_ind = pixel[0] as f64 * 0.0353;
         let rampabs = ramp_ind.round();
 
         let ascichar = ramp.as_bytes()[rampabs as usize] as char;
-        println!("{}", ascichar);
+        if prev_y != y {
+            print!("\n");
+            print!("{}", ascichar);
+        } else {
+            print!("{}", ascichar);
+        }
+
+        prev_y = y;
 
         // in this loop if ramp_ind = pixel[i] *0.0353
         // and print ramp[ramp_ind] sort of thig ?
     }
-    println!("{}", ramp.len());
+    // println!("{}", ramp.len());
 
     Ok(())
 }
