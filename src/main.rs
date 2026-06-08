d2271dc446bbbc259882246b428a4126bc31b7ad
Akshit Vig <akshitvig48@gmail.com>
2026-06-08 16:10:59 +0530

    resizing done

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
-use image::{GenericImageView, ImageReader};
+use image::{GenericImageView, ImageReader, imageops::FilterType::Gaussian};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = ImageReader::open("toji.jpg")?.decode()?;
+    let mut img = ImageReader::open("toji.jpg")?.decode()?;
 
-    // println!("width: {}", img.width());
-    // println!("height: {}", img.height());
+    print!("{:?}", img.dimensions());
+
+    img = image::DynamicImage::resize(&img, 150, 75, Gaussian);
 
     let gray = img.grayscale();
 
     let ramp = " .:-=+*#%@";
 
     let mut prev_y = 0;
+    let magic_num = (ramp.len() - 1) as f64 / 255.0;
 
     for (x, y, pixel) in gray.pixels() {
-        // println!("{}, {} {},  {:?}", i, x, y, pixel[0]);
-        let ramp_ind = pixel[0] as f64 * 0.0353;
-        let rampabs = ramp_ind.round();
+        let ramp_ind = pixel[0] as f64 * magic_num;
+        let ramp_abs = ramp_ind.round();
+        let asciichar = ramp.as_bytes()[ramp_abs as usize] as char;
 
-        let ascichar = ramp.as_bytes()[rampabs as usize] as char;
         if prev_y != y {
             print!("\n");
-            print!("{}", ascichar);
+            print!("{}", asciichar);
         } else {
-            print!("{}", ascichar);
+            print!("{}", asciichar);
         }
 
         prev_y = y;
-
-        // in this loop if ramp_ind = pixel[i] *0.0353
-        // and print ramp[ramp_ind] sort of thig ?
     }
-    // println!("{}", ramp.len());
 
     Ok(())
 }
